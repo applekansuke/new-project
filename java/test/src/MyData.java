e001318c76d99eb7d01cc26c10536161bfce1d43
Daisuke Kanno <applekansuke123@gmail.com>
2017-02-06 13:05:23 +0900

    ソースコード形成

--- java/test/src/MyData.java
+++ java/test/src/MyData.java
@@ -8,39 +8,39 @@ import com.example.tool.DumpObject;
 
 public class MyData {
 	// map
-	Map <String, DumpObject> map = new HashMap<String, DumpObject>();
+	Map<String, DumpObject> map = new HashMap<String, DumpObject>();
 	// map‚Ìkey
 	DefaultListModel model = new DefaultListModel();
 	// map‚Ìvalue
-    DumpObject dumper;
-    Sample sample;
-    
-    MyData(){
-		
+	DumpObject dumper;
+	Sample sample;
+
+	MyData() {
+
 		sample = new Sample("hello");
-        dumper = new DumpObject(sample);
+		dumper = new DumpObject(sample);
 		String lTmp = MyTime.getString() + " Windows";
-        map.put(lTmp, dumper);
+		map.put(lTmp, dumper);
 		model.addElement(lTmp);
-       
-        sample = (Sample) sample.clone();
+
+		sample = (Sample) sample.clone();
 		dumper = new DumpObject(sample);
 		lTmp = MyTime.getString() + " Linux";
-        map.put(lTmp, dumper);
+		map.put(lTmp, dumper);
 		model.addElement(lTmp);
-        sample.name += " daisuke";
+		sample.name += " daisuke";
 
 		lTmp = MyTime.getString() + " Other";
 		model.addElement(lTmp);
-		
-        sample = (Sample) sample.clone();
-        dumper = new DumpObject(sample);
+
+		sample = (Sample) sample.clone();
+		dumper = new DumpObject(sample);
 		lTmp = MyTime.getString() + " Mac OS X";
-        map.put(lTmp, dumper);
+		map.put(lTmp, dumper);
 		model.addElement(lTmp);
-        sample.name += " ahoaho!";
+		sample.name += " ahoaho!";
 
 		lTmp = MyTime.getString() + " Other";
 		model.addElement(lTmp);
-    }
+	}
 }
