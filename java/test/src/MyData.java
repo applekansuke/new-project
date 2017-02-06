82a225749a9d22aa8951bd8986b787552933a8e2
Daisuke Kanno <applekansuke123@gmail.com>
2017-02-06 16:26:52 +0900

    fff追加

--- java/test/src/MyData.java
+++ java/test/src/MyData.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import javax.swing.DefaultListModel;
 
 import com.example.tool.DumpObject;
-/// ddd
+/// fff
 public class MyData {
 	// map
 	Map<String, DumpObject> map = new HashMap<String, DumpObject>();
